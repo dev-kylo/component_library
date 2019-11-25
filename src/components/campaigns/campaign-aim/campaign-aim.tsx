3afbe7f2c1e740c4d79853d608d31212b9a407ba
Kyle Robinson <kylerobbo12@gmail.com>
2019-11-25 16:36:04 +0000

    page content container

--- src/components/campaigns/campaign-aim/campaign-aim.tsx
+++ src/components/campaigns/campaign-aim/campaign-aim.tsx
@@ -8,13 +8,13 @@ import { Component, h } from '@stencil/core';
 export class CampaignAim {
   render() {
     return (
-        <page-container>
+        <page-content>
             <h2>Campaign Aim</h2>
             <p><slot></slot></p>
             <p>Sign the petition, vote to end the assessment cap to achieve fairer assessments for all.</p>
             <div class="buttonCTA purpleBorder icon like"><a href="https://www.facebook.com/groups/1758956314405344/" target="_blank">Like</a></div>
             <div class="buttonCTA purpleBorder icon share" id="fb-share-button"><span>Share</span></div>
-        </page-container>
+        </page-content>
     );
   }
 }
