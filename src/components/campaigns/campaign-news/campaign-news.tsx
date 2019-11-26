149088d6dbb8b4d1f4f7a26c6154295d44c0b0de
Kyle Robinson <kylerobbo12@gmail.com>
2019-11-26 16:49:24 +0000

    server route

--- src/components/campaigns/campaign-news/campaign-news.tsx
+++ src/components/campaigns/campaign-news/campaign-news.tsx
@@ -12,7 +12,7 @@ export class CampaignNews {
   @State() testdata;
 
   componentDidLoad() {
-    fetch('https://kclsu.org/svc/feeds/news/6013?subtree=true')
+    fetch('https://kclsu-heroku.herokuapp.com/newslist/17592')
         .then(res => res.json())
         .then(newsData => {
             this.data = newsData;
@@ -21,7 +21,7 @@ export class CampaignNews {
 
   @Method()
   async fetchNews() {
-    fetch('https://kclsu.org/svc/feeds/news/6013?subtree=true')
+    fetch('https://kclsu-heroku.herokuapp.com/newslist/17592')
     .then(res => res.json())
     .then(newsData => {
         this.testdata = newsData;
