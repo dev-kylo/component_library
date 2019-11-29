408a95c5ba50df46fbddae662c8f4f1deebc1a09
Kyle Robinson <kylerobinson@Kyles-MacBook-Pro.local>
2019-11-29 11:02:51 +0000

    campaign tabs markup switch

--- src/components/campaigns/campaign-tabs/campaign-tabs.tsx
+++ src/components/campaigns/campaign-tabs/campaign-tabs.tsx
@@ -11,7 +11,8 @@ export class CampaignTabs {
     return (
         <page-content>
             <tabs-container>
-                <single-tab tabheading="UPDATES">
+                <h2>UPDATES</h2>
+                <div>
                     <section class="col-md-6">
                         <div class="news cardBorder green container">
                             <div class="mobileView news_full">
@@ -28,23 +29,26 @@ export class CampaignTabs {
                             <div class="fb-group" data-href="" data-show-metadata="false" data-show-social-context="true" data-width="320">&nbsp;</div>
                         </div>
                     </section>
-                </single-tab>
+                </div>
 
-                <single-tab tabheading="OVERVIEW">
+                <h2>OVERVIEW</h2>
+                <div>
                     <slot></slot>
-                </single-tab>
-                <single-tab tabheading="HOW IT WORKS">
+                </div>
+
+                <h2>HOW IT WORKS</h2>
+                <div>
                     <h3>How do campaigns work?</h3>
                     <p>For a campaign to be officially supported by KCLSU, it needs to have the support of our student membership which is done through an online petition. Add your name to the petition to support the campaign.</p>
                     <p><em>To support this campaign as an association member, please update your eligibility on your voting profile.</em>&nbsp;You can find out more information about how to update your voting profile <a href="https://www.kclsu.org/elections/eligibilityexplained/">here</a></p>
                     <p>Campaigns need to be supported by <em>150 KCLSU members</em> to become a KCLSU supported campaign <em>or 75 Association members</em> to become a KCLSU Association Supported campaign.</p>
                     <p>Once the threshold is reached, it means the campaign will receive guidance and support from KCLSU staff to create a strategy and deliver the campaign aim. </p>
                     <p>To find out more about this campaign and how to get involved, be sure to&nbsp;join up!</p>
-                </single-tab>
-                {/* <single-tab tabheading="DOCUMENTS">
+                </div>
+                {/* <div tabheading="DOCUMENTS">
                     <h3>You must be logged in as a student to view these documents.</h3>
                     
-                </single-tab> */}
+                </div> */}
             </tabs-container>
         </page-content>
     );
