edd8fc7f1bdb53554b74ef6ee5a77e4197a5f473
Kyle Robinson <kylerobinson@Kyles-MacBook-Pro.local>
2019-11-29 16:19:55 +0000

    setwell class

--- src/components/campaigns/show-support/support-progress/support-progress.tsx
+++ src/components/campaigns/show-support/support-progress/support-progress.tsx
@@ -10,13 +10,12 @@ export class SupportProgress {
   render() {
     return (
         <div class="col-md-6">
-            <div class="well">
+            <div class="well setwell">
                 <h5>Disabled Association Members</h5>
                 <div aria-valuemax="75" aria-valuemin="0" aria-valuenow="3" class="ui-progressbar ui-corner-all ui-widget ui-widget-content">
                     <div class="setcaption">3 of 75</div>
                 </div>
                 <p>Petition Closed</p>
-                <p></p>
             </div>
         </div>
     );
