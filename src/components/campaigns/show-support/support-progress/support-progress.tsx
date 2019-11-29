b8f4211ce474aab3978f5943a783937de6f4e442
Kyle Robinson <kylerobinson@Kyles-MacBook-Pro.local>
2019-11-29 17:49:43 +0000

    add class well

--- src/components/campaigns/show-support/support-progress/support-progress.tsx
+++ src/components/campaigns/show-support/support-progress/support-progress.tsx
@@ -22,7 +22,7 @@ export class SupportProgress {
             <div class="well setwell">
                 <h5>Disabled Association Members</h5>
                 <div aria-valuemax="75" aria-valuemin="0" aria-valuenow="3" class="ui-progressbar ui-corner-all ui-widget ui-widget-content">
-                    <div class="setcaption">3 of 75</div>
+                    <div class="setcaption caption">3 of 75</div>
                     <div class="ui-progressbar-value ui-corner-left ui-widget-header"></div>
                 </div>
                 <p>Petition Closed</p>
