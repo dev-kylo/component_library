ba67bb013049346e401df8321994a1d30996736d
Kyle Robinson <kylerobinson@Kyles-MacBook-Pro.local>
2019-11-29 17:33:59 +0000

    set style in js

--- src/components/campaigns/show-support/support-progress/support-progress.tsx
+++ src/components/campaigns/show-support/support-progress/support-progress.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from '@stencil/core';
+import { Component,h, Element} from '@stencil/core';
 
 @Component({
   tag: 'support-progress',
@@ -7,13 +7,23 @@ import { Component, h } from '@stencil/core';
 
 export class SupportProgress {
 
+    @Element() el: HTMLElement;
+    private progressBar: HTMLElement;
+
+    componentDidLoad(){
+        this.progressBar = this.el.querySelector('.ui-progressbar-value');
+        this.progressBar.style.width = '64%';
+    }
+
   render() {
+
     return (
         <div class="col-md-6">
             <div class="well setwell">
                 <h5>Disabled Association Members</h5>
                 <div aria-valuemax="75" aria-valuemin="0" aria-valuenow="3" class="ui-progressbar ui-corner-all ui-widget ui-widget-content">
                     <div class="setcaption">3 of 75</div>
+                    <div class="ui-progressbar-value ui-corner-left ui-widget-header"></div>
                 </div>
                 <p>Petition Closed</p>
             </div>
