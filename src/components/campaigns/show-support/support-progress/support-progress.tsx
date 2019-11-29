4ad0eec96fb4d6380677b59cd458bf5fa1d239aa
Kyle Robinson <kylerobinson@Kyles-MacBook-Pro.local>
2019-11-29 18:26:58 +0000

    set progress bar values

--- src/components/campaigns/show-support/support-progress/support-progress.tsx
+++ src/components/campaigns/show-support/support-progress/support-progress.tsx
@@ -1,4 +1,4 @@
-import { Component,h, Element} from '@stencil/core';
+import { Component,h, Element, Prop} from '@stencil/core';
 
 @Component({
   tag: 'support-progress',
@@ -8,11 +8,14 @@ import { Component,h, Element} from '@stencil/core';
 export class SupportProgress {
 
     @Element() el: HTMLElement;
+    @Prop() maxtotal: string;
+    @Prop() current: string;
+    @Prop() supportname: string;
     private progressBar: HTMLElement;
 
     componentDidLoad(){
         this.progressBar = this.el.querySelector('.ui-progressbar-value');
-        this.progressBar.style.width = '64%';
+        this.progressBar.style.width = this.current / this.maxtotal * 100;
     }
 
   render() {
@@ -20,8 +23,8 @@ export class SupportProgress {
     return (
         <div class="col-md-6">
             <div class="well setwell">
-                <h5>Disabled Association Members</h5>
-                <div aria-valuemax="75" aria-valuemin="0" aria-valuenow="3" class="ui-progressbar ui-corner-all ui-widget ui-widget-content">
+                <h5> {this.supportname} </h5>
+                <div aria-valuemax={this.maxtotal} aria-valuemin="0" aria-valuenow={this.current} class="ui-progressbar ui-corner-all ui-widget ui-widget-content">
                     <div class="setcaption caption">3 of 75</div>
                     <div class="ui-progressbar-value ui-corner-left ui-widget-header"></div>
                 </div>
