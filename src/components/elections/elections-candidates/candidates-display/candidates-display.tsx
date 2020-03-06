ce02dabfa5fdb1160282af07dd00f6f9f8739a1b
Kyle Robinson <kylerobinson@Kyles-MBP.Home>
2020-03-06 15:13:23 +0200

    change manifesto title

--- src/components/elections/elections-candidates/candidates-display/candidates-display.tsx
+++ src/components/elections/elections-candidates/candidates-display/candidates-display.tsx
@@ -18,7 +18,7 @@ export class CandidatesDisplay {
                         position={candidate.Post}
                         link={candidate.ManifestoLink}
                         image={candidate.ImageLink? candidate.ImageLink : 'https://res.cloudinary.com/kclsu-media/image/upload/f_auto,fl_any_format,g_center,q_100/v1581516201/website_uploads/KCLSU%20Brand/Bzcl1r6L_400x400_se7grm.jpg'} 
-                        cta='My manifesto'
+                        cta='Manifesto'
                         secondcta = 'Breakdown'
                         secondlink = {candidate.ResultsLink}
                     />
