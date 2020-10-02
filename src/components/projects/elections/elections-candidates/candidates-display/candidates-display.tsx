6bb9c0568a6cc2253626e9cfda763d6e638a9c05
KCLSU <45687039+KCLSU@users.noreply.github.com>
2020-10-02 13:34:23 +0200

    tweaks

--- src/components/projects/elections/elections-candidates/candidates-display/candidates-display.tsx
+++ src/components/projects/elections/elections-candidates/candidates-display/candidates-display.tsx
@@ -12,34 +12,36 @@ export class CandidatesDisplay {
     @Prop() data;
     
     render() {
-        const cards = !this.data? null : this.data
-                                                .sort((a, b) => {
-                                                    if (a.DisplayName){
-                                                        let nameA = a.DisplayName.split(' ');
-                                                        let nameB = b.DisplayName.split(' ');
-                                                        if (nameA[1] < nameB[1]) {
-                                                            return -1;
-                                                          }
-                                                        if (nameA[1]  > nameB[1] ) {
-                                                        return 1;
-                                                        }
-                                                    }
-                                                    
-                                                })
-                                                .map(candidate =>{ 
-                                                    let name = candidate.Name || candidate.DisplayName;
-                                                    let image = candidate.ImageLink || candidate.ImageUrl;
-                                                    if (name.includes('R.O.N')) name = 'Re-Open Nominations';
-                                                    return <profile-card 
-                                                                name={name.toLowerCase()}
-                                                                position={candidate.Post.Title || candidate.Post}
-                                                                link={candidate.ManifestoLink || candidate.ManifestoUrl}
-                                                                image={image} 
-                                                                cta='Manifesto'
-                                                                secondcta = 'Breakdown'
-                                                                secondlink = {candidate.ResultsLink}
-                                                            />
-                                                })
+        const cards = !this.data? null
+         : 
+         this.data
+                .sort((a, b) => {
+                    if (a.DisplayName){
+                        let nameA = a.DisplayName.split(' ');
+                        let nameB = b.DisplayName.split(' ');
+                        if (nameA[1] < nameB[1]) {
+                            return -1;
+                            }
+                        if (nameA[1]  > nameB[1] ) {
+                        return 1;
+                        }
+                    }
+                    
+                })
+                .map(candidate =>{ 
+                    let name = candidate.Name || candidate.DisplayName;
+                    let image = candidate.ImageLink || candidate.ImageUrl;
+                    if (name.includes('R.O.N')) name = 'Re-Open Nominations';
+                    return <profile-card 
+                                name={name.toLowerCase()}
+                                position={candidate.Post.Title || candidate.Post}
+                                link={candidate.ManifestoLink || candidate.ManifestoUrl}
+                                image={image} 
+                                cta='Manifesto'
+                                secondcta = 'Breakdown'
+                                secondlink = {candidate.ResultsLink}
+                            />
+                })
 
         return (
             <profile-card-layout>
